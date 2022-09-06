5d31e36fa59a5f7f61fe0e5d1863b8e69f48e6ee
Fellipe Lopes <fellipe561@gmail.com>
2022-09-05 23:54:55 -0300

    criação da lógica para deixar invisivel um tipo de cadastro enquanto se estiver usando outro

--- js/script_cadastro_lista.js
+++ js/script_cadastro_lista.js
@@ -1,31 +1,30 @@
 function abrir(res){
+    switch (res){
+        case 1: //ficha de inscrição
+            
+            break;
+        
+        case 2: //aluno
+            var verificacao = document.getElementById('aluno').classList.contains("mostrar");
+            
+            if(verificacao == false){
+                document.getElementById('aluno').classList.add("mostrar");
+                excluir("pai");
+            }
+            break;
+        
+        case 3: //pai
+            var verificacao = document.getElementById('pai').classList.contains("mostrar");
 
-
-switch (res){
-    case 1:
-        
-        break;
-    
-    case 2:
-        var verificacao = document.getElementById('aluno').classList.contains("mostrar");
-        
-        if(verificacao == false){
-            document.getElementById('aluno').classList.add("mostrar");
-            document.getElementById('pai').classList.remove("mostrar");
-        }
-        break;
-    
-    case 3:
-        var verificacao = document.getElementById('pai').classList.contains("mostrar");
-        
-        if(verificacao == false){
-            document.getElementById('aluno').classList.remove("mostrar");
-            document.getElementById('pai').classList.add("mostrar");
-        }
-        
-        break;
-    
+            if(verificacao == false){
+                document.getElementById('pai').classList.add("mostrar");
+                excluir("aluno");
+            }
+            break; 
+    }
 }
 
+function excluir(str){
+    document.getElementById(str).classList.remove("mostrar");
 }
 
